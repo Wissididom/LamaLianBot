74192f430efc1b1862fbce880d1d088ec14e7d0b
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-10-01 15:01:53 +0200

    Change from `.avatarURL()` to `displayAvatarURL({ dynamic: true })`

--- logging/user-update.js
+++ logging/user-update.js
@@ -48,7 +48,7 @@ export default async function handleUserUpdate(oldUser, newUser) {
   if (oldUser.avatar != newUser.avatar) {
     embed.addFields({
       name: "Avatar",
-      value: `[Old](<${oldUser.avatarURL()}>) -> [New](<${newUser.avatarURL()}>)`,
+      value: `[Old](<${oldUser.displayAvatarURL({ dynamic: true })}>) -> [New](<${newUser.displayAvatarURL({ dynamic: true })}>)`,
       inline: true,
     });
     fields++;
