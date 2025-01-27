2516fd6ea577ea92f71131235f4927bdb312bdde
Wissididom <30803034+Wissididom@users.noreply.github.com>
2025-01-27 09:34:00 +0100

    Add username to moderator field of guild-ban-add

--- logging/guild-ban-add.js
+++ logging/guild-ban-add.js
@@ -31,7 +31,7 @@ export default async function handleGuildBanAdd(ban) {
           },
           {
             name: "Moderator",
-            value: banner ? `<@${banner.id}> (${banner.displayName})` : "N/A",
+            value: banner ? `<@${banner.id}> (${banner.displayName} - ${banner.username})` : "N/A",
             inline: true,
           },
           {
