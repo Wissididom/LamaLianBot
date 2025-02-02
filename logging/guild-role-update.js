d2e90a26334e3e072ad034d523f08ec0c696264a
Wissididom <30803034+Wissididom@users.noreply.github.com>
2025-02-02 22:33:16 +0100

    Update role stuff

--- logging/guild-role-update.js
+++ logging/guild-role-update.js
@@ -1,4 +1,4 @@
-import { EmbedBuilder, Events } from "discord.js";
+import { AuditLogEvent, EmbedBuilder, Events } from "discord.js";
 import { getChannelByEventName } from "../logging.js";
 
 export default async function handleGuildRoleUpdate(oldRole, newRole) {
@@ -7,6 +7,7 @@ export default async function handleGuildRoleUpdate(oldRole, newRole) {
     Events.GuildRoleUpdate,
   );
   if (!logChannel) return; // Don't handle event, if logChannel is not set
+  let fieldsAdded = false;
   const embed = new EmbedBuilder()
     .setTitle("Rolle bearbeitet")
     .setDescription(`**Rolle <@&${newRole.id}> (${newRole.name}) bearbeitet**`)
@@ -18,6 +19,7 @@ export default async function handleGuildRoleUpdate(oldRole, newRole) {
       value: `${oldRole.hexColor} -> ${newRole.hexColor}`,
       inline: true,
     });
+    fieldsAdded = true;
   }
   if (oldRole.hoist != newRole.hoist) {
     embed.addFields({
@@ -25,6 +27,7 @@ export default async function handleGuildRoleUpdate(oldRole, newRole) {
       value: `${oldRole.hoist ? "Ja" : "Nein"} -> ${newRole.hoist ? "Ja" : "Nein"}`,
       inline: true,
     });
+    fieldsAdded = true;
   }
   if (oldRole.icon != newRole.icon) {
     embed.addFields({
@@ -32,6 +35,7 @@ export default async function handleGuildRoleUpdate(oldRole, newRole) {
       value: `[altes Icon](${oldRole.iconURL()}) -> [neues Icon](${newRole.iconURL()})`,
       inline: true,
     });
+    fieldsAdded = true;
   }
   if (oldRole.managed != newRole.managed) {
     embed.addFields({
@@ -39,6 +43,7 @@ export default async function handleGuildRoleUpdate(oldRole, newRole) {
       value: `${oldRole.managed ? "Ja" : "Nein"} -> ${newRole.managed ? "Ja" : "Nein"}`,
       inline: true,
     });
+    fieldsAdded = true;
   }
   if (oldRole.mentionable != newRole.mentionable) {
     embed.addFields({
@@ -46,6 +51,7 @@ export default async function handleGuildRoleUpdate(oldRole, newRole) {
       value: `${oldRole.mentionable ? "Ja" : "Nein"} -> ${newRole.mentionable ? "Ja" : "Nein"}`,
       inline: true,
     });
+    fieldsAdded = true;
   }
   if (oldRole.name != newRole.name) {
     embed.addFields({
@@ -53,6 +59,15 @@ export default async function handleGuildRoleUpdate(oldRole, newRole) {
       value: `\`${oldRole.name}\` -> \`${newRole.name}\``,
       inline: true,
     });
+    fieldsAdded = true;
+  }
+  if (oldRole.position != newRole.position) {
+    embed.addFields({
+      name: "Position",
+      value: `#${oldRole.position} -> #${newRole.position}`,
+      inline: true,
+    });
+    fieldsAdded = true;
   }
   if (oldRole.permissions != newRole.permissions) {
     const oldArray = oldRole.permissions.toArray();
@@ -63,18 +78,39 @@ export default async function handleGuildRoleUpdate(oldRole, newRole) {
       embed.addFields({
         name: "Rechte",
         value: `${oldArray && oldArray.length > 0 ? oldJoined : "Keine Rechte"} -> ${newArray && newArray.length > 0 ? newJoined : "Keine Rechte"}`,
-        inline: true,
+        inline: false,
       });
+      fieldsAdded = true;
     }
   }
-  if (oldRole.position != newRole.position) {
+  const updater = await fetchUpdater(newRole);
+  if (updater) {
     embed.addFields({
-      name: "Position",
-      value: `#${oldRole.position} -> #${newRole.position}`,
-      inline: true,
+      name: "Moderator",
+      value: `<@${updater.id}> (\`${updater.displayName}\` - \`${updater.username}\` - \`${updater.id}\`)`,
+      inline: false,
     });
+    fieldsAdded = true;
   }
+  if (!fieldsAdded) return;
   await logChannel.send({
     embeds: [embed],
   });
+}
+
+async function fetchUpdater(role) {
+  const fetchedLogs = await role.guild.fetchAuditLogs({
+    limit: 1,
+    type: AuditLogEvent.RoleUpdate,
+  });
+  const roleLog = fetchedLogs.entries.first();
+  if (!roleLog) {
+    return null;
+  }
+  const { executor, target } = roleLog;
+  if (target.id == role.id) {
+    return executor;
+  } else {
+    return null;
+  }
 }
